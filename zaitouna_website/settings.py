f02ca46afcc6ef4b3281efe720c3936bc041f478
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-14 14:58:53 +0200

    migrate to postgressql

--- zaitouna_website/settings.py
+++ zaitouna_website/settings.py
@@ -13,6 +13,8 @@ import os
 import sys
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
@@ -120,12 +122,16 @@ WSGI_APPLICATION = 'zaitouna_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
 CSRF_TRUSTED_ORIGINS = [
     "https://*.herokuapp.com"
 ]
