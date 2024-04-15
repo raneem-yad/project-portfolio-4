7590ec01a69cb1c09097e1aa1b4029f6d174e564
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-15 20:54:08 +0200

    fix unstyled elements in search page

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Profiles, EditProfile
 
 urlpatterns = [
-    path('user/<slug:pk>/', Profiles.as_view(), name='profile'),
-    path('edit/<slug:pk>/', EditProfile.as_view(), name='edit_profile')
+    path('user/<int:pk>/', Profiles.as_view(), name='profile'),
+    path('edit/<int:pk>/', EditProfile.as_view(), name='edit_profile')
 ]
