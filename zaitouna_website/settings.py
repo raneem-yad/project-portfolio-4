8e60d749903497024b1ac7282943e10c37b3e0bc
raneem-yad <eng.raneem.yad@gmail.com>
2024-04-18 09:22:27 +0200

    fix bookmarked recipe counts

--- zaitouna_website/settings.py
+++ zaitouna_website/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # if exist then true if not then false
-DEBUG = True
+DEBUG = False
 # DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [".herokuapp.com", "127.0.0.1", ".gitpod.io"]
